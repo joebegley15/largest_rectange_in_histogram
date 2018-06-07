84d4d1ffe95dfb70be878ed9fe33d407fc95f8c6
Joe Begley <joebegley15@gmail.com>
2018-06-07 15:45:46 -0400

    Added working solution

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-var largestRect(arr) {
+function largestRect(arr) {
 	var largest = 0;
-	for ()
+	for (var i = 0; i < arr.length; i++) {
+		var tempCount = 1;
+		for (var x = i-1; x >= 0; x--) {
+			if (arr[x] >= arr[i]) {
+				tempCount++
+			} else {
+				x = 0;
+			}
+		}
+		for (var y = i+1; y < arr.length; y++) {
+			if (arr[y] >= arr[i]) {
+				tempCount++;
+			} else {
+				y = arr.length;
+			}
+		}
+		var thisLargest = tempCount * arr[i];
+		if (thisLargest > largest) {
+			largest = thisLargest
+		}
+	}
+	return largest;
 }
 
-largestRect([3, 5, 12, 4, 10])
-largestRect([6, 2, 5, 4, 5, 1, 6])
-largestRect([9, 7, 5, 4, 2, 5, 6, 7, 7, 5, 7, 6, 4, 4, 3, 2])
+console.log(largestRect([3, 5, 12, 4, 10]));
+console.log(largestRect([6, 2, 5, 4, 5, 1, 6]));
+console.log(largestRect([9, 7, 5, 4, 2, 5, 6, 7, 7, 5, 7, 6, 4, 4, 3, 2]));
